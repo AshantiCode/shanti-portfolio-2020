b872feb441edeaf9b67c9a135d7e0ced1f04acc2
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-08 20:34:40 +0200

    add lotties, finish web styling contact

--- js/script.js
+++ js/script.js
@@ -1,5 +1,70 @@
-let linkedin = bodymovin.loadAnimation({
-	container: document.getElementById("linkedin"),
-	render: "svg",
-	path: "linkedin.json"
+
+
+const linkedin = document.getElementById("linkedin");
+const github = document.getElementById("github");
+const mail = document.getElementById("mail");
+const flower = document.getElementById("flower");
+
+let animation1 = bodymovin.loadAnimation({
+	container: linkedin,
+	renderer: "svg",
+	path: "js/linkedin.json",
+	autoplay: false,
+	loop: true,
+	speed: 0.5,
 });
+
+
+let animation2 = bodymovin.loadAnimation({
+	container: github,
+	renderer: "svg",
+	path: "js/github-transp.json",
+	autoplay: false,
+	loop: true,
+	// speed: 0.5,
+});
+
+let animation3 = bodymovin.loadAnimation({
+	container: mail,
+	renderer: "svg",
+	path: "js/email.json",
+	autoplay: false,
+	loop: true,
+	speed: 0.5,
+});
+
+let animation4 = bodymovin.loadAnimation({
+	container: flower,
+	renderer: "svg",
+	path: "js/flower.json",
+	autoplay: true,
+	loop: true,
+	speed: 0.5,
+});
+
+
+
+linkedin.addEventListener("mouseenter", function () {
+	animation1.play();
+});
+
+linkedin.addEventListener("mouseleave", function () {
+	animation1.stop();
+});
+
+github.addEventListener("mouseenter", function () {
+	animation2.play();
+});
+
+github.addEventListener("mouseleave", function () {
+	animation2.stop();
+});
+
+mail.addEventListener("mouseenter", function () {
+	animation3.play();
+});
+
+mail.addEventListener("mouseleave", function () {
+	animation3.stop();
+});
+
