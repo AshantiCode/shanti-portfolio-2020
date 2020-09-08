44fa6ba92ba6bf61e4614f00251362801fd884e1
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-09 01:34:39 +0200

    add smooth scrolling and gsap

--- js/script.js
+++ js/script.js
@@ -1,90 +1,100 @@
+// Smooth Scrolling
+(function () {
+	const scroll = new LocomotiveScroll({
+		el: document.querySelector(".smoothScrolling"),
+		smooth: true
+	});
+})();
+
+// Lottie Animations
+window.onload = function () {
+	const linkedin = document.getElementById("linkedin");
+	const github = document.getElementById("github");
+	const mail = document.getElementById("mail");
+	const flower = document.getElementById("flower");
+	const hello = document.getElementById("hello");
+	const scroll = document.getElementById("arrow-scroll");
+
+	let animation1 = bodymovin.loadAnimation({
+		container: linkedin,
+		renderer: "svg",
+		path: "js/linkedin.json",
+		autoplay: false,
+		loop: true,
+		speed: 0.5,
+	});
 
 
-const linkedin = document.getElementById("linkedin");
-const github = document.getElementById("github");
-const mail = document.getElementById("mail");
-const flower = document.getElementById("flower");
-const hello = document.getElementById("hello");
-const scroll = document.getElementById("arrow-scroll");
+	let animation2 = bodymovin.loadAnimation({
+		container: github,
+		renderer: "svg",
+		path: "js/github-transp.json",
+		autoplay: false,
+		loop: true,
+		// speed: 0.5,
+	});
 
-let animation1 = bodymovin.loadAnimation({
-	container: linkedin,
-	renderer: "svg",
-	path: "js/linkedin.json",
-	autoplay: false,
-	loop: true,
-	speed: 0.5,
-});
+	let animation3 = bodymovin.loadAnimation({
+		container: mail,
+		renderer: "svg",
+		path: "js/email.json",
+		autoplay: false,
+		loop: true,
+		speed: 0.5,
+	});
 
+	let animation4 = bodymovin.loadAnimation({
+		container: flower,
+		renderer: "svg",
+		path: "js/flower.json",
+		autoplay: true,
+		loop: true,
+		speed: 0.5,
+	});
 
-let animation2 = bodymovin.loadAnimation({
-	container: github,
-	renderer: "svg",
-	path: "js/github-transp.json",
-	autoplay: false,
-	loop: true,
-	// speed: 0.5,
-});
+	let animation5 = bodymovin.loadAnimation({
+		container: hello,
+		renderer: "svg",
+		path: "js/hello.json",
+		autoplay: true,
+		loop: false,
 
-let animation3 = bodymovin.loadAnimation({
-	container: mail,
-	renderer: "svg",
-	path: "js/email.json",
-	autoplay: false,
-	loop: true,
-	speed: 0.5,
-});
+	});
+	animation5.setSpeed(0.5)
 
-let animation4 = bodymovin.loadAnimation({
-	container: flower,
-	renderer: "svg",
-	path: "js/flower.json",
-	autoplay: true,
-	loop: true,
-	speed: 0.5,
-});
-
-let animation5 = bodymovin.loadAnimation({
-	container: hello,
-	renderer: "svg",
-	path: "js/hello.json",
-	autoplay: true,
-	loop: false,
-
-});
-animation5.setSpeed(0.5)
-
-let animation6 = bodymovin.loadAnimation({
-	container: scroll,
-	renderer: "svg",
-	path: "js/downward-circle.json",
-	autoplay: true,
-	loop: true,
-});
+	let animation6 = bodymovin.loadAnimation({
+		container: scroll,
+		renderer: "svg",
+		path: "js/downward-circle.json",
+		autoplay: true,
+		loop: true,
+	});
 
 
 
-linkedin.addEventListener("mouseenter", function () {
-	animation1.play();
-});
+	linkedin.addEventListener("mouseenter", function () {
+		animation1.play();
+	});
 
-linkedin.addEventListener("mouseleave", function () {
-	animation1.stop();
-});
+	linkedin.addEventListener("mouseleave", function () {
+		animation1.stop();
+	});
 
-github.addEventListener("mouseenter", function () {
-	animation2.play();
-});
+	github.addEventListener("mouseenter", function () {
+		animation2.play();
+	});
 
-github.addEventListener("mouseleave", function () {
-	animation2.stop();
-});
+	github.addEventListener("mouseleave", function () {
+		animation2.stop();
+	});
 
-mail.addEventListener("mouseenter", function () {
-	animation3.play();
-});
+	mail.addEventListener("mouseenter", function () {
+		animation3.play();
+	});
 
-mail.addEventListener("mouseleave", function () {
-	animation3.stop();
-});
+	mail.addEventListener("mouseleave", function () {
+		animation3.stop();
+	});
 
+
+}
