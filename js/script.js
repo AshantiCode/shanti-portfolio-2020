c54fd4b9c1671f55869734347a5aa3c82a47da70
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-10 22:33:30 +0200

    fix smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 // Smooth Scrolling
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 // Smooth Scrolling not working with Scroll Trigger
 (function () {
 	const scroll = new LocomotiveScroll({
-		el: document.querySelector('[data-scroll-container]'),
+		el: document.querySelector('.smooth-scroll'),
 		smooth: true
 	});
 })();
 
+
+
 scroll.on("scroll", ScrollTrigger.update);
 // Tell Scrollrigger to use this proxy
-ScrollTrigger.scrollerProxy(".scrollContainer", {
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
 	scrollTop(value) {
 		return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
 	},
 	getBoundingClientRect() {
 		return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
 	},
-	pinType: document.querySelector(".scrollContainer").style.transform ? "transform" : "fixed"
+	pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
 });
 
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
